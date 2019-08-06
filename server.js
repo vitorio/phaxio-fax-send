105e10d537138f714f2dd355f05cf309db9d544a
Glitch (hello-express) <none>
2019-08-06 05:21:54 +0000

    🐪❤️ Checkpoint ./server.js:234254/1512 ./.glitch-assets:234254/385

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-// server.js
-// where your node app starts
-
 // init project
 const express = require("express");
 const app = express();
 const Twilio = require("twilio");
-
-const cfg = {
-  port: process.env.PORT || 3000,
-  secret: process.env.APP_SECRET || "keyboard cat",
-  twilioAccountSid: process.env.TWILIO_ACCOUNT_SID,
-  twilioAuthToken: process.env.TWILIO_AUTH_TOKEN,
-  twilioPhoneNumber: process.env.TWILIO_PHONE_NUMBER
-};
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
@@ -22,20 +11,24 @@ app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/boop", function(req, res, next) {
-  const client = new Twilio(cfg.twilioAccountSid, cfg.twilioAuthToken);
+app.get("/sms/:secret", function(req, res, next) {
+  if (req.params.secret !== process.env.SECRET) {
+    res.status(403);
+    res.end('incorrect secret!');
+    return;
+  }
+  const client = new Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
   // Create options to send the message
   const options = {
-    to: cfg.myPhoneNumber,
-    from: cfg.twilioPhoneNumber,
+    to: process.env.YOUR_PHONE_NUMBER,
+    from: process.env.TWILIO_PHONE_NUMBER,
     body: `boop!`
   };
 
   // Send the message!
   client.messages.create(options, function(err, response) {
     if (err) {
-      // Just log it for now
       console.error(err);
       res.end('oh no');
     } else {
@@ -45,6 +38,16 @@ app.get("/boop", function(req, res, next) {
   });
 });
 
+// https://cdn.glitch.com/6f4fae79-2dd2-46da-996b-fd5b029d3171%2Ffish-image.png?v=1565068857990
+/* 
+client.messages
+      .create({
+         body: 'Hello there!',
+         from: '+15555555555',
+         mediaUrl: ['https://demo.twilio.com/owl.png'],
+         to: '+12316851234'
+       })
+      .then(message => console.log(message.sid));*/
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
