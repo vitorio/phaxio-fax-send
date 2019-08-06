336bae7cf4db648cb4249f9aabfbb7a9bf99cf76
Glitch (hello-express) <none>
2019-08-06 05:31:55 +0000

    🕋💰 Checkpoint ./server.js:234254/1345 ./views/index.html:234254/762

--- server.js
+++ server.js
@@ -4,15 +4,21 @@ const app = express();
 const Twilio = require("twilio");
 
 // http://expressjs.com/en/starter/static-files.html
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(function (req, res, next) {
+  console.log(req.method, req.url);
+  next();
+});
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/sms/:secret", function(req, res, next) {
-  if (req.params.secret !== process.env.SECRET) {
+app.post("/sms", function(req, res, next) {
+  if (req.body.secret !== process.env.SECRET) {
     res.status(403);
     res.end('incorrect secret!');
     return;
@@ -38,16 +44,33 @@ app.get("/sms/:secret", function(req, res, next) {
   });
 });
 
-// https://cdn.glitch.com/6f4fae79-2dd2-46da-996b-fd5b029d3171%2Ffish-image.png?v=1565068857990
-/* 
-client.messages
-      .create({
-         body: 'Hello there!',
-         from: '+15555555555',
-         mediaUrl: ['https://demo.twilio.com/owl.png'],
-         to: '+12316851234'
-       })
-      .then(message => console.log(message.sid));*/
+app.post("/mms", function(req, res, next) {
+  if (req.body.secret !== process.env.SECRET) {
+    res.status(403);
+    res.end('incorrect secret!');
+    return;
+  }
+  const client = new Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+  // Create options to send the message
+  const options = {
+    to: process.env.YOUR_PHONE_NUMBER,
+    from: process.env.TWILIO_PHONE_NUMBER,
+    mediaUrl: ['https://demo.twilio.com/owl.png'],
+    body: `hoot!`
+  };
+
+  // Send the message!
+  client.messages.create(options, function(err, response) {
+    if (err) {
+      console.error(err);
+      res.end('oh no');
+    } else {
+      console.log('success!');
+      res.end('success!');
+    }
+  });
+});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
