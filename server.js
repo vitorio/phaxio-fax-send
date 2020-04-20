faa7b12fe6350cedda70941c696f7b3cccba02d1
Glitch (hello-express) <none>
2020-04-20 19:39:35 +0000

    📒🚛 Checkpoint ./server.js:15177034/1791

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 // init project
 const express = require("express");
+
+const multer  = require('multer')
+const tmpDir = '/tmp/image-resize'
+const upload = multer({ dest: tmpDir })
+
+const mimetype = {
+  'image/png'  : 'png',
+  'image/jpeg' : 'jpg',
+}
+
 const app = express();
 const Twilio = require("twilio");
 
@@ -40,6 +50,69 @@ app.get("/setup-status", function (req, res) {
     "twilio-phone": !!process.env.TWILIO_PHONE_NUMBER
   });
 });
+
+app.post("/", upload.single('image'), (req, res, next) => {
+  // check to see if we support this filetype
+  if ( !(req.file.mimetype in mimetype) ) {
+    return res.sendStatus(400)
+  }
+
+  const ext = mimetype[req.file.mimetype]
+  const filename = req.file.path
+  const outfile = req.file.path + '.' + ext
+
+  // check width and height
+  const width  = req.body.width|0
+  const height = req.body.height|0
+  
+  // if we have both a width and height, then resize independent of original dimensions
+  // else use one 
+  if ( width && height ) {
+    
+  }
+  else if ( width ) {
+    if ( width < 1 || width > 1280 ) {
+      return res.status(403).send('Width must be between 1 and 1280.')
+    }
+    
+  }
+  else if ( height ) {
+    if ( height < 1 || height > 1280 ) {
+      return res.status(403).send('Height must be between 1 and 1280.')
+    }
+    
+  }
+  else {
+     // nothing, so return an error 
+    return res.status(403).send('Provide one of width or height or both.')
+  }
+
+  // console.log('file:', req.file)
+
+  sharp(req.file.path)
+    .rotate()
+    .resize(width || null, height || null)
+    .toFile(outfile)
+    .then(() => {
+      res.sendFile(outfile, (err) => {
+        setTimeout(() => {
+          fs.unlink(filename, (err) => console.log)
+        }, 5 * 1000)
+        if (err) return next(err)
+        console.log('Sent:', outfile)
+        setTimeout(() => {
+          fs.unlink(outfile, (err) => console.log)
+        }, 5 * 1000)
+      })
+    })
+    .catch(err => {
+      setTimeout(() => {
+        fs.unlink(filename, (err) => console.log)
+      }, 5 * 1000)
+      next(err) 
+     })
+  ;
+})
 
 app.post("/mms", function(req, res) {
    if (!req.body.secret || req.body.secret !== process.env.SECRET) {
