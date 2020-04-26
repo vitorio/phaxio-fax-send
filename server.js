f895f3333f2b51f1315d8bec0f60d30ba7541fb9
Glitch (hello-express) <none>
2020-04-26 23:53:38 +0000

    🐸💒 Checkpoint ./views/index.html:15177034/660 ./.glitch-assets:15177034/3254 ./views/setup.html:15177034/1473 ./server.js:15177034/287

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.use('/fax-files', function(req, res, next) {
   next();
 });
 app.use('/fax-files', express.static('/tmp/faxfiles'))
+app.use('/favicon.ico', express.static('/public/favicon.ico'))
 
 app.get("/", function(req, res) {
   // show the setup page if the env isn't configured
