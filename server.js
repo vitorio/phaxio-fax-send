aa508191c7c4612b4222d5db345e216e24ddc5e0
Glitch (adaptive-efficacious-dream) <none>
2020-05-08 23:02:43 +0000

    Rewound to commit ec45aa60dfb0a1ff2e120a536549ce983cc7fd85 + 0 edits

--- server.js
+++ server.js
@@ -25,10 +25,27 @@ app.use(fileUpload({
   preserveExtension : true
 }));
 
+// Twilio.webhook() doesn't seem to properly validate fax GET requests, so doing them by hand
+app.use('/fax-files', function(req, res, next) {
+  var url = 'https://' + req.hostname + req.originalUrl;
+  var sig = crypto.createHmac('sha1', process.env.PHAXIOTOKEN).update(Buffer.from(url, 'utf-8')).digest('hex');
+  
+  console.error(req.headers);
+
+  if (req.header('X-Phaxio-Signature') !== sig) {
+    console.error(url + ' requested without Phaxio sig');
+    return res.status(403).send('File requested without Phaxio signature.');
+  }
+
+  next();
+});
+app.use('/fax-files', express.static('/tmp/faxfiles'))
+
 app.get("/", function(req, res) {
   // show the setup page if the env isn't configured
   if (process.env.PHAXIOKEY &&
       process.env.PHAXIOSECRET &&
+      process.env.PHAXIOTOKEN &&
       process.env.SECRET) {
     res.sendFile(__dirname + "/views/index.html");
   } else {
@@ -44,7 +61,7 @@ app.get("/setup", function(req, res) {
 app.get("/setup-status", function (req, res) {
   res.json({
     "secret": !!process.env.SECRET,
-    "credentials": !!(process.env.PHAXIOKEY && process.env.PHAXIOSECRET)
+    "credentials": !!(process.env.PHAXIOKEY && process.env.PHAXIOSECRET && process.env.PHAXIOTOKEN)
   });
 });
 
@@ -61,30 +78,32 @@ app.post("/send-fax", function(req, res) {
     return res.status(400).send('No destination phone number was provided.');
   }
   
-  /* ???
+  if (req.body.quality !== "standard" && req.body.quality !== "fine" && req.body.quality !== "superfine") {
+    return res.status(400).send('Invalid quality.');
+  }  
+
   if (req.files.fax.mimetype != "application/pdf") {
     return res.status(415).send('Uploaded file doesn\'t look like a PDF.')
   }
-  */
   
   const phaxio = new Phaxio(process.env.PHAXIOKEY, process.env.PHAXIOSECRET);
   
-  // Send a single fax 
-  phaxio.faxes.create({
+  // Create options to send the message
+  const options = {
     to: req.body.to,
-    file: req.files.fax.tempFilePath
-  })
-  .then((fax) => {
-    // The `create` method returns a fax object with methods attached to it for doing things
-    // like cancelling, resending, getting info, etc.
+    content_url: 'https://' + req.hostname + req.files.fax.tempFilePath.replace('/tmp/faxfiles', '/fax-files')
+  };
 
-    // Wait 5 seconds to let the fax send, then get the status of the fax by getting its info from the API.
-    return setTimeout(() => {
-      res.redirect('/fax-status?id=' + fax.id);
-    }, 5000)
-  })
-  .then(status => console.log('Fax status response:\n', JSON.stringify(status, null, 2)))
-  .catch((err) => { throw err; });
+  // Send the message!
+  phaxio.faxes.create(options, function(err, response) {
+    if (err) {
+      console.error(err);
+      res.end('oh no, there was a fax sending error! Check the app logs for more information.');
+    } else {
+      console.log(options.content_url);
+      res.redirect('/fax-status?id=' + response.id);
+    }
+  });
 });
 
 app.get("/fax-status", function(req, res) {
@@ -95,14 +114,17 @@ app.get("/fax-status", function(req, res) {
   const phaxio = new Phaxio(process.env.PHAXIOKEY, process.env.PHAXIOSECRET);
   
   // check status
-  phaxio.faxes.getInfo(req.query.id)
-  .then(response => {
+  client.fax.faxes(req.query.id).fetch(function(err, response) {
+    if (err) {
+      console.error(err);
+      res.end('oh no, there was a fax status error! Check the app logs for more information.');
+    } else {
       var price = '$0.00';
-      if (response.data.cost) price = '$' + (response.data.cost);
-      res.end(response.data.num_pages + ' page(s) submitted ' + response.data.created_at + ' is/are ' + response.data.status + ' costing ' + price + ' (refresh for updates)');
-      if (response.data.status == 'failure') console.error(response);
-  })
-  .catch((err) => { throw err; });
+      if (response.price) price = '$' + (response.price * -1.0);
+      res.end(response.numPages + ' page(s) submitted ' + response.dateCreated + ' is/are ' + response.status + ' in ' + response.quality + ' quality costing ' + price + ' (refresh for updates)');
+      if (response.status == 'failed') console.error(response);
+    }
+  });
 });
 
 // listen for requests :)
